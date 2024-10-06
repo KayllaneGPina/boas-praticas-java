b83bc4e3fbf4b71d9bbf2a257aef0ecb0973b629
Kayllane Pina <kayllanegfpina@gmail.com>
2024-10-06 17:22:51 -0300

    feat: Conhecendo testes de unidade

---
+++ src/test/java/br/com/alura/service/AbrigoServiceTest.java
@@ -0,0 +1,48 @@
+package br.com.alura.service;
+
+import br.com.alura.client.ClientHttpConfig;
+import br.com.alura.domain.Abrigo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.http.HttpResponse;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AbrigoServiceTest {
+
+    private ClientHttpConfig clienteHttpConfig = mock(ClientHttpConfig.class);
+    private AbrigoService abrigoService = new AbrigoService(clienteHttpConfig);
+    private HttpResponse<String> response = mock(HttpResponse.class);
+    private Abrigo abrigo = new Abrigo("Teste", "123456789", "abrigo_alura");
+
+    @Test
+    public void deveVerificarSeDispararRequisicaoGetSeraChamado() throws IOException, InterruptedException {
+        abrigo.setId(0L);
+        String expectedAbrigosCadastrados = "Abrigos cadastrados:";
+        String expectedIdENome = "0 - Teste";
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        when(response.body()).thenReturn("[{"+abrigo.toString()+"}]");
+        when(clienteHttpConfig.disparaRequisicaoGet(anyString())).thenReturn(response);
+
+        abrigoService.listarAbrigo();
+
+        String[] lines = baos.toString().split(System.lineSeparator());
+        String actualAbrigosCadastrados = lines[0];
+        String actualIdENome = lines[1];
+
+        Assertions.assertEquals(expectedAbrigosCadastrados, actualAbrigosCadastrados);
+        Assertions.assertEquals(expectedIdENome, actualIdENome);
+
+    }
+
+}
