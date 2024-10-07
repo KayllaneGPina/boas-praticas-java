d4dc0727580e82af0a4ed7d1a25543c7de461ab4
Kayllane Pina <kayllanegfpina@gmail.com>
2024-10-07 19:50:07 -0300

    feat: Extraindo envio de emails

---
+++ aula-2/3238-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/service/EmailService.java
@@ -0,0 +1,26 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.model.Adocao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void enviarEmail(String to, String subject, String menssage) {
+
+        SimpleMailMessage email = new SimpleMailMessage();
+
+        email.setFrom("adopet@email.com.br");
+        email.setTo(to);
+        email.setSubject(subject);
+        email.setText(menssage);
+        emailSender.send(email);
+    }
+
+}
