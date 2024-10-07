f78ac8063c50fb240d80736447092b45661bc9c5
Kayllane Pina <kayllanegfpina@gmail.com>
2024-10-07 19:19:34 -0300

    feat: Extraindo camada Service

---
+++ aula-2/3238-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/execption/ValidacaoException.java
@@ -0,0 +1,7 @@
+package br.com.alura.adopet.api.execption;
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String message) {
+        super(message);
+    }
+}
